4060f33a313c429b27d56015a0ac90e39cd1dd1b
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2021-12-27 17:20:09 -0300

    Primeiro Commit

---
+++ src/test/java/com/example/melifood/MeliFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.melifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
